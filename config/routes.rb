6e733aec700f657ac3c2c3c16baa6aba52e45dec
Donnell Reuben <donnell.reuben@icloud.com>
2023-03-29 13:49:25 -0500

    made first product, Cheetos. img_url is image_url

--- config/routes.rb
+++ config/routes.rb
@@ -5,3 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   get "product", controller: "product", action: "product"
 end
+
+
+# product = Product.new(name: "Cheetos", price: 3, image_url: "https://www.cheetos.com/sites/cheetos.com/files/2019-03/Cheetos%20Crunchy_v2_0.png", description: "Bring a cheesy, delicious crunch to snack time with a bag of, CHEETOS, Crunchy Cheese-Flavored Snacks. Made with real cheese for maximum flavor.")
+                      
