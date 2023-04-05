bcc5c779e3dac92a47247af9e7ae7b54807e54b2
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-05 11:27:19 -0500

    pushing

---
+++ db/migrate/20230404191106_change_description_datatype_in_products.rb
@@ -0,0 +1,5 @@
+class ChangeDescriptionDatatypeInProducts < ActiveRecord::Migration[7.0]
+  def change
+    change_column :products, :description, :text
+  end
+end
---
+++ db/migrate/20230404191936_change_price_datatype_in_products.rb
@@ -0,0 +1,5 @@
+class ChangePriceDatatypeInProducts < ActiveRecord::Migration[7.0]
+  def change
+    change_column :products, :price, :decimal
+  end
+end
