c98689710ece01265cffca1dd29486e0b2f5bd0c
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-05 11:42:03 -0500

    installed seed_dump and dumped all info into seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Product.create!([
+  {name: "Coke", price: "2.0", image_url: "https://imgs.search.brave.com/gUG6nrXZefSNqCaSS_MDFRBbiWaNQEdJY2jrb-W9ReE/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9pbWFn/ZXMtbmEuc3NsLWlt/YWdlcy1hbWF6b24u/Y29tL2ltYWdlcy9J/LzcxTkt3b0VHQlZM/LmpwZw", description: "an American brand of sports-themed beverage and food products, built around its signature line of sports drinks", inventory: nil},
+  {name: "Cheetos", price: "3.0", image_url: "https://www.cheetos.com/sites/cheetos.com/files/2019-03/Cheetos%20Crunchy_v2_0.png", description: " a cheesy, delicious crunch to snack time with a bag of CHEETOS® Crunchy Cheese-Flavored Snacks. Made with real cheese for maximum flavor.", inventory: nil}
+])
