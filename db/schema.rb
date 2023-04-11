7bd6550ce76e848fdfffea8cef08d5cca21ea755
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-11 11:42:39 -0500

    made a new model - images

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_11_163848) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_11_164142) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_images_on_product_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -32,4 +40,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_11_163848) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "images", "products"
 end
